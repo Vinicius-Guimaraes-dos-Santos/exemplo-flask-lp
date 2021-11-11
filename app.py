c33185e1e074a73b6008c3e7c1996c885c896290
Lucas Alixame <lucasali2003@gmail.com>
2021-11-11 15:46:42 +0000

    Adicionando Material

--- app.py
+++ app.py
@@ -11,30 +11,59 @@ vendas = [
 
 @app.route('/')
 def index():
+     """
+        Renderizando Tela: INDEX
+        Passando variavel: lista(tipo lista de dados)
+    """
     return render_template('index.html', lista=vendas)
 
 @app.route('/create')
 def create():
+    """
+        Renderizando Tela: CREATE
+    """
     return render_template('create.html')
 
 @app.route('/save', methods=['POST'])  # <form action="/save" method="POST">
 def save():
+    """
+        Pegando Dados do Formulario
+    """
     name = request.form['name']          # <input name="name"/>
     product = request.form['product']    # <input name="product"/>
     price = request.form['price']        # <input name="price"/>
 
+    """
+        Aplicando REPLACE no preço (trocando , por .)
+    """
     price = price.replace(",", ".")
 
+    """
+        Identificando ultimo elemento dentro da lista
+    """
     ultimo = vendas[-1]
 
+    """
+        Moldando Lista e definindo dados
+        OBS: Ao definir o 'id' do registro, utilizamos o ultimo elemento da lista somando o valor de seu id + 1
+    """
     venda = {"id": ultimo["id"] + 1,"name": name, "product": product, "price": float(price)}
+
+    """
+        Adicionando elemento a lista
+    """
     vendas.append(venda)
 
+    """
+        Redirecionando para a pagina principal
+    """
     return redirect(URL + '/')
 
 @app.route('/delete/<id>')
 def delete(id):
-    del(vendas[id - 1])
+    index = int(id)
+
+    del vendas[index - 1]
 
     return redirect(URL + '/')
 
